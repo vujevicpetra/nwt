646f93473488b80538ad9b9f3c68dd3de9494e36
Petra <petravujevic00@gmail.com>
2022-07-13 22:05:38 +0300

    Added

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { AppBar, Typography, Toolbar, Avatar, Button } from '@material-ui/core';
+import { Link, useHistory, useLocation } from 'react-router-dom';
+
+import useStyles from './styles';
+import review from '../../images/review.png';
+
+const Navbar = () => {
+    const classes = useStyles();
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+
+    useEffect(() => {
+        const token = user?.token;
+    
+        setUser(JSON.parse(localStorage.getItem('profile')));
+      }, []);
+
+    return (
+        <AppBar className={classes.appBar} position="static" color="inherit">
+            <div className={classes.brandContainer}>
+                <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Review</Typography>
+                <img className={classes.image} src={review} alt="review" height="60" />
+            </div>
+            <Toolbar className={classes.toolbar}>
+                {user ? (
+                <div className={classes.profile}>
+                    <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
+                    <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+                    <Button variant="contained" className={classes.logout} color="secondary" >Logout</Button>
+                </div>
+                ) : (
+                    <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+                )}
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Navbar;
